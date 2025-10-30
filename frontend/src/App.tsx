b9b54252d2eca3b15a0e3ee46a6472da8e71c85f
yminghua <yminghua8@gmail.com>
2025-10-30 00:54:48 -0400

    config route protection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,32 @@
+import { AuthContext } from './context/AuthContext';
+import { useContext, type JSX } from 'react';
+import { HashRouter, Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import TravelPlanPage from './pages/TravelPlanPage';
 
 export default function App() {
-  return <LoginPage />;
+  const { currentUser } = useContext(AuthContext);
+
+  const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+    return children;
+  };
+
+  return (
+    <HashRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <TravelPlanPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </HashRouter>
+  );
 }
