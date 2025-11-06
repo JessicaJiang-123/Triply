3c459cbc34b8964c48a4e5f3a29477869600602d
Minghua Yang <minghuay@andrew.cmu.edu>
2025-11-06 06:56:26 -0500

    update route path for AddPlacePage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import LoginPage from './pages/LoginPage';
 import TravelPlanPage from './pages/TravelPlanPage';
 import AddTripPage from './pages/AddTripPage';
 import AddPlacePage from './pages/AddPlacePage';
+import PlanDetailPage from './pages/PlanDetailPage';
 
 export default function App() {
   const { currentUser } = useContext(AuthContext);
@@ -28,6 +29,22 @@ export default function App() {
             </ProtectedRoute>
           }
         />
+        <Route
+          path="/trips/:trip_id/days/:day_id"
+          element={
+            <ProtectedRoute>
+              <PlanDetailPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/trips/:trip_id/days/:day_id/places/:place_id/edit-place"
+          element={
+            <ProtectedRoute>
+              <AddPlacePage />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/login" element={<LoginPage />} />
         <Route
           path="/add-trip"
@@ -38,7 +55,7 @@ export default function App() {
           }
         />
         <Route
-          path="/trips/:trip_id/:date/add-place"
+          path="/trips/:trip_id/days/:day_id/add-place"
           element={
             <ProtectedRoute>
               <AddPlacePage />
