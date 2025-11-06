0325b5fb745a0a754e4df880cd556c9813833d3e
Minghua Yang <minghuay@andrew.cmu.edu>
2025-11-06 06:58:18 -0500

    refactor apis into users app and plans app

--- backend/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("user/profile/", views.user_profile, name="user_profile"),
-    path("user/logout/", views.user_logout, name="user_logout"),
-]
